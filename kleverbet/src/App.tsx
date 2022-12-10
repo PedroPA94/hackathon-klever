816a2edaad8e789cbead00c39b983194a9f6cd40
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2022-12-10 14:15:59 -0300

    feat: implemented game controls, WIP(input validation)

--- kleverbet/src/App.tsx
+++ kleverbet/src/App.tsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import GameControl from './components/GameControl/GameControl'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <GameControl />
     </div>
   )
 }
--- kleverbet/src/components/GameControl/GameControl.tsx
+++ kleverbet/src/components/GameControl/GameControl.tsx
@@ -3,15 +3,13 @@ import { SubmitHandler, useForm } from 'react-hook-form';
 
 type Inputs = {
   betValue: number;
-  multiplier: number;
 }
 
-function GameControl() {
-  const { register, handleSubmit, watch, formState: { errors }, setValue, getValues } = useForm<Inputs>({
+const GameControl = () => {
+  const { register, handleSubmit, formState: { errors, isValid }, setValue, getValues } = useForm<Inputs>({
     defaultValues: {
       betValue: 0,
-      multiplier: 1,
-    }
+    },
   });
 
   const doubleBetValue = ():void => {
@@ -23,19 +21,17 @@ function GameControl() {
     const betValue = getValues('betValue');
     setValue('betValue', Math.floor(betValue / 2))
   }
+  
+  const onSubmit: SubmitHandler<Inputs> = (data) => localStorage.setItem('user', JSON.stringify(data));
 
-  console.log(getValues('betValue'))
-  const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input type="number" {...register("betValue", { required: true })} />
+    <form onSubmit={ handleSubmit(onSubmit) }>
+      <span>0.00</span>
+      <input type="text" { ...register("betValue", { required: true }) } />
+      { errors.betValue && <span>This field is required</span> }
       <button type="button" onClick={ doubleBetValue }>2X</button>
       <button type="button" onClick={ halveBetValue }>1/2</button>
-      { errors.betValue && <span>This field is required</span>}
-      <input type="number" {...register("multiplier", { required: true })} />
-      { errors.multiplier && <span>This field is required</span>}
-      
-      <button type="submit">BET</button>
+      <button type="submit" disabled={ !isValid } >BET</button>
     </form>
   );
 }
