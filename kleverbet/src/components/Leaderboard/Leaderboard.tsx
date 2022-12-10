0bc01c08df897e8fbbeec0c54c4126cddcefc7a2
Danubio Rafael <danubio.rafaeel@gmail.com>
2022-12-10 19:26:42 -0300

    feat: add leaderboard records

--- kleverbet/src/components/Leaderboard/Leaderboard.tsx
+++ kleverbet/src/components/Leaderboard/Leaderboard.tsx
@@ -63,6 +63,47 @@ const Leaderboard = (): React.ReactElement => {
     won: boolean,
   }
 
+  const getGreaterMultiplier = () => {
+    let greaterMultiplier = 0;
+
+    scoreboard.forEach((score) => {
+      if(score.multiplier > greaterMultiplier && score.won) greaterMultiplier = score.bet;
+    });
+
+    return greaterMultiplier;
+  }
+
+  const getGreaterProfit = () => {
+    let greaterProfit = 0;
+
+    scoreboard.forEach((score) => {
+      if(score.bet > greaterProfit && score.won) greaterProfit = score.bet;
+    });
+
+    return greaterProfit
+  }
+
+  const winsInARowCalculator = () => {
+    const victoriesInARow: number[] = [];
+    let greaterNumberOfVictories = 0;
+
+    scoreboard.reduce((acc, score) => {
+      if(score.won) {
+        return acc + 1
+      } else {
+        victoriesInARow.push(acc);
+        return 0;
+      }
+
+    }, 0)
+
+    victoriesInARow.forEach((victories) => {
+      if(victories > greaterNumberOfVictories) greaterNumberOfVictories = victories;
+    })
+
+    return greaterNumberOfVictories;
+  }
+
   const profitCalculator = () => {
     return scoreboard.reduce((acc, score) => {
       if(score.won) return acc + (score.bet * score.multiplier);
@@ -95,8 +136,15 @@ const Leaderboard = (): React.ReactElement => {
     <section className="leaderboard-section">
       <div className="leaderboard-header">
         <p><strong>You</strong> bet {scoreboard.length} times</p>
+        <aside className="leaderboard-records">
+          <h3>Records</h3>
+          <p>Wins in a row: {winsInARowCalculator()}</p>
+          <p>Greater Profit: {getGreaterProfit()}</p>
+          <p>Better Multiplier: {getGreaterMultiplier()}</p>
+        </aside>
         <p>Total Profit: <strong>{profitCalculator()} TC</strong></p>
       </div>
+      
       <table className="table">
         <tr className="table-header-row">
           <th>Bet</th>
