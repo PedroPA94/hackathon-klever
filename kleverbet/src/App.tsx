e7cee19b18baa4235fc6aec76124a091939fe176
Pedro Papini <ppapinia@gmail.com>
2022-12-11 07:34:28 -0300

    feat: add notification toast when user wins or loses

--- kleverbet/src/App.tsx
+++ kleverbet/src/App.tsx
@@ -1,12 +1,15 @@
 import Header from './components/Header';
 import './App.css';
 import GameContainer from './components/GameContainer';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
       <GameContainer />
+      <ToastContainer />
     </div>
   )
 }
